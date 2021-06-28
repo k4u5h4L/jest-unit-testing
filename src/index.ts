ba3aeea011d67bf2b114a6e79267056cd4be6978
k4u5h4L <kaushal.v.bhat@gmail.com>
2021-06-28 18:48:57 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+type QueueType = string | number;
+
+class Queue {
+    queue: QueueType[];
+
+    constructor() {
+        this.queue = [];
+    }
+
+    get all(): QueueType[] {
+        return this.queue;
+    }
+
+    get top(): QueueType {
+        return this.queue[0];
+    }
+
+    pop(): void {
+        this.queue.shift();
+    }
+
+    push(value: QueueType): void {
+        this.queue.push(value);
+    }
+}
+
+// const q = new Queue();
+
+// q.push(1);
+// q.push(2);
+// q.push(3);
+
+// console.log(q.all);
+
+// q.pop();
+
+// console.log(q.all);
+
+export { Queue };
---
+++ src/test/queue.test.ts
@@ -0,0 +1,31 @@
+import { Queue } from "../index";
+
+describe("My Stack", () => {
+    let q;
+
+    beforeEach(() => {
+        q = new Queue();
+    });
+
+    it("is created empty", () => {
+        expect(q.queue).toEqual([]);
+    });
+
+    it("can push from rear", () => {
+        q.push(1);
+        q.push(2);
+        q.push("three");
+        expect(q.all).toEqual([1, 2, "three"]);
+    });
+
+    it("can pop from front", () => {
+        q.push(1);
+        q.push(2);
+        q.push("three");
+
+        q.pop();
+        expect(q.top).toBe(2);
+    });
+
+    it.todo("can destruct after");
+});
